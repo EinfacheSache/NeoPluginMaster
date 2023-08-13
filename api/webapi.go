cc8b94f5888ab9211b688c2140e39a4663aba6ae
EinfacheSache <klugetommy@web.de>
2023-08-13 18:47:44 +0200

    Changes -> Added pluginVersion + recode + timeout after 30s instead of 20s

--- api/webapi.go
+++ api/webapi.go
@@ -3,6 +3,7 @@ package api
 import (
 	"encoding/json"
 	"fmt"
+	"github.com/prometheus/client_golang/prometheus"
 	"golang.org/x/time/rate"
 	"log"
 	"neo-plugin-master/exporter"
@@ -38,41 +39,37 @@ var BackendStatsMutex = new(sync.RWMutex)
 var ServerCount = float64(0)
 var PlayerCount = float64(0)
 
+var limiter = rate.NewLimiter(rate.Every(1*time.Second/30), 1)
+
 func Run() {
-	http.Handle("/api/stats/plugin", pluginMetricsWithRateLimit(pluginMetrics))
+	http.HandleFunc("/api/stats/plugin", pluginMetricsFailedHandler)
 	fmt.Println("API listener started at 0.0.0.0:8080")
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
 
-func pluginMetricsWithRateLimit(next func(w http.ResponseWriter, r *http.Request)) http.Handler {
-	limiter := rate.NewLimiter(rate.Every(1*time.Second/50), 50)
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		if r.Method != http.MethodPost {
-			http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
-			fmt.Println("Method not allowed")
-			return
+func pluginMetricsFailedHandler(w http.ResponseWriter, r *http.Request) {
+
+	if r.Method != http.MethodPost {
+		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
+		fmt.Println("Method not allowed")
+		return
+	}
+
+	if !limiter.Allow() {
+		message := ResponseMessage{
+			Status: "Rate limit exceeded",
+			Body:   "You are being rate limitted. Please try again later.",
 		}
 
-		if !limiter.Allow() {
-			message := ResponseMessage{
-				Status: "Rate limit exceeded",
-				Body:   "You are being rate limitted. Please try again later.",
-			}
-
-			w.WriteHeader(http.StatusTooManyRequests)
-			err := json.NewEncoder(w).Encode(&message)
-			if err != nil {
-				http.Error(w, err.Error(), http.StatusInternalServerError)
-			}
-			fmt.Println("rate limit exceeded")
-			return
-		} else {
-			next(w, r)
+		w.WriteHeader(http.StatusTooManyRequests)
+		err := json.NewEncoder(w).Encode(&message)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
 		}
-	})
-}
+		fmt.Println("rate limit exceeded")
+		return
+	}
 
-func pluginMetrics(w http.ResponseWriter, r *http.Request) {
 	var statsRequest stats
 	statsRequest.backendID = r.Header.Get("backendID")
 	if statsRequest.backendID == "" {
@@ -81,63 +78,79 @@ func pluginMetrics(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	err := json.NewDecoder(r.Body).Decode(&statsRequest)
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusBadRequest)
+	err2 := json.NewDecoder(r.Body).Decode(&statsRequest)
+	if err2 != nil {
+		http.Error(w, err2.Error(), http.StatusBadRequest)
 		fmt.Println("request failed: formatted error")
 		return
 	}
-
 	w.WriteHeader(http.StatusOK)
+
+	pluginMetrics(statsRequest)
+}
+
+func pluginMetrics(statsRequest stats) {
+	fmt.Println("ID (", statsRequest.backendID, ")", " PlayerCount(", statsRequest.PlayerAmount, ")")
+
 	statsRequest.latestPing = time.Now().UnixMilli()
 
 	BackendStatsMutex.RLock()
 	latestStats, ok := BackendStats[statsRequest.backendID]
 	BackendStatsMutex.RUnlock()
-
-	fmt.Println("ID (", statsRequest.backendID, ")", " PlayerCount(", statsRequest.PlayerAmount, ")")
-
 	if ok {
 		PlayerCount -= latestStats.PlayerAmount
 		ServerCount -= 1
+		labels := prometheus.Labels{
+			"server_version": latestStats.ServerVersion,
+		}
+		exporter.PluginVersion.DeletePartialMatch(labels)
 	}
+
 	PlayerCount += statsRequest.PlayerAmount
 	ServerCount += 1
+	labels := prometheus.Labels{
+		"server_version": statsRequest.ServerVersion,
+	}
+
+	exporter.PluginVersion.With(labels).Inc()
+	exporter.PlayerAmount.Set(PlayerCount)
+	exporter.ServerAmount.Set(ServerCount)
 
 	BackendStatsMutex.Lock()
 	BackendStats[statsRequest.backendID] = statsRequest
 	BackendStatsMutex.Unlock()
-
-	exporter.PlayerAmount.Set(PlayerCount)
-	exporter.ServerAmount.Set(ServerCount)
 
 	go startTimeout(statsRequest.backendID)
 }
 
 func startTimeout(backendID string) {
 
-	time.Sleep(time.Second * 20)
+	time.Sleep(time.Second * 30)
 
 	BackendStatsMutex.RLock()
-	stats, ok := BackendStats[backendID]
+	lastStats, ok := BackendStats[backendID]
 	BackendStatsMutex.RUnlock()
 	if !ok {
 		fmt.Printf("cant found key in map %s\n", backendID)
 		return
 	}
 
-	if time.Now().UnixMilli()-stats.latestPing < 1000*20 {
+	if time.Now().UnixMilli()-lastStats.latestPing < 1000*30 {
 		// Server did not timeout and send ping in latest 40 sec -> dont delete
 		return
 	}
 
-	PlayerCount -= stats.PlayerAmount
+	PlayerCount -= lastStats.PlayerAmount
 	ServerCount -= 1
+	labels := prometheus.Labels{
+		"server_version": lastStats.ServerVersion,
+	}
+
+	exporter.PlayerAmount.Set(PlayerCount)
+	exporter.ServerAmount.Set(ServerCount)
+	exporter.PluginVersion.DeletePartialMatch(labels)
 
 	BackendStatsMutex.Lock()
 	delete(BackendStats, backendID)
 	BackendStatsMutex.Unlock()
-
-	exporter.PlayerAmount.Set(PlayerCount)
-	exporter.ServerAmount.Set(ServerCount)
 }
--- exporter/prometheusexporter.go
+++ exporter/prometheusexporter.go
@@ -11,6 +11,7 @@ import (
 
 var PlayerAmount prometheus.Gauge
 var ServerAmount prometheus.Gauge
+var PluginVersion *prometheus.CounterVec
 
 func Run() {
 	http.Handle("/metrics", promhttp.Handler())
@@ -26,13 +27,10 @@ func Run() {
 	})
 	ServerAmount.Set(0)
 
-	/*
-		playerCounter = promauto.NewCounterVec(prometheus.CounterOpts{
-			Name: "plugin_online_player",
-			Help: "show the amount of players online",
-		}, []string{"online_player"})
-
-	*/
+	PluginVersion = promauto.NewCounterVec(prometheus.CounterOpts{
+		Name: "plugin_server_versions",
+		Help: "show the version of the servers",
+	}, []string{"server_version"})
 
 	fmt.Println("PrometheusExporter started at 0.0.0.0:8069")
 	log.Fatal(http.ListenAndServe(":8069", nil))
