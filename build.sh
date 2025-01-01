ade54c128c7a6ab0fc0524a08ece83e324e4d840
EinfacheSache <klugetommy@web.de>
2025-01-01 02:20:04 +0000

    Edit build.sh

--- build.sh
+++ build.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 
-# Zielplattform definieren (Standard: Linux ARM64)
-GOOS=${GOOS:-linux}   # Betriebssystem
-GOARCH=${GOARCH:-arm64} # Architektur (arm64 für 64-Bit, arm für 32-Bit)
-GOARM=${GOARM:-7}      # ARM-Version (nur für 32-Bit ARM relevant)
+# # Zielplattform definieren (Standard: Linux ARM64)
+# GOOS=${GOOS:-linux}   # Betriebssystem
+# GOARCH=${GOARCH:-arm64} # Architektur (arm64 für 64-Bit, arm für 32-Bit)
+# GOARM=${GOARM:-7}      # ARM-Version (nur für 32-Bit ARM relevant)
 
-# Git-Commit-Hash für Debugging
-COMMIT=$(git rev-parse HEAD)
+# # Git-Commit-Hash für Debugging
+# COMMIT=$(git rev-parse HEAD)
 
-# Statisches Go-Binary bauen
-CGO_ENABLED=0 GOOS=$GOOS GOARCH=$GOARCH GOARM=$GOARM go build -ldflags="-s -w -X main.Commit=$COMMIT"
+# # Statisches Go-Binary bauen
+# CGO_ENABLED=0 GOOS=$GOOS GOARCH=$GOARCH GOARM=$GOARM go build -ldflags="-s -w -X main.Commit=$COMMIT"#!/bin/bash
+
+CGO_ENABLED=0 go build -ldflags="-s -w -X main.Commit=$(git rev-parse HEAD)"
